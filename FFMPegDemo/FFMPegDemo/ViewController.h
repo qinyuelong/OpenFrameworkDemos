9baa9602bbc7dd7c0c0655b53cd3220c86cf0fcd
qinyuelong <qinyuelong.cn@gmail.com>
2017-08-06 15:21:29 +0800

    添加 GPUImage 与 FFMpeg 共同作用的 测试项目

---
+++ FFMPegDemo/FFMPegDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFMPegDemo
+//
+//  Created by qinge on 2017/8/6.
+//  Copyright © 2017年 qin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FFMPegDemo/FFMPegDemo/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  FFMPegDemo
+//
+//  Created by qinge on 2017/8/6.
+//  Copyright © 2017年 qin. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <GPUImage.h>
+#import "avformat.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
